8a04787dee6b0f81d24577345333a6c7e0e808ac
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2024-08-12 21:24:45 +0900

    Taskドメインオブジェクトのテストを追加

--- src/domain/Task/tests/Task.test.ts
+++ src/domain/Task/tests/Task.test.ts
@@ -1,9 +1,31 @@
 import { describe, expect, it } from "vitest";
-import { TaskName } from "../Task";
+import { Task, TaskId, TaskName } from "../Task";
+import { UserId } from "@/domain/User";
+import { experimental_taintUniqueValue } from "react";
+
+describe("Task", () => {
+  describe("create", () => {
+    it("タスクの作成に成功する", () => {
+      const taskRaw = {
+        id: new TaskId(),
+        name: new TaskName("name"),
+        userId: new UserId(),
+        dueDate: new Date(),
+      };
+      const actual = new Task(
+        taskRaw.id,
+        taskRaw.name,
+        taskRaw.userId,
+        taskRaw.dueDate
+      );
+      expect(actual).toEqual(expect.objectContaining(taskRaw));
+    });
+  });
+});
 
 describe("TaskName", () => {
   describe("constructor", () => {
-    it("TaskNameインスタンスの生成に成功する", () => {
+    it("インスタンスの生成に成功する", () => {
       const actual = new TaskName("name");
       expect(actual).toBeInstanceOf(TaskName);
       expect(actual.value).toBe("name");
