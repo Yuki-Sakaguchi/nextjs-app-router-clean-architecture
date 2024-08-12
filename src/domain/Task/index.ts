ee07206b8ca2be2c565eff42c396a03d9c8f5eab
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2024-08-12 16:18:22 +0900

    Taskのドメインを実装

---
+++ src/domain/Task/index.ts
@@ -0,0 +1,50 @@
+import { v4 as uuidv4 } from "uuid";
+
+import { UserId } from "../User";
+
+/**
+ * タスク
+ */
+export class Task {
+  constructor(
+    public id: TaskId,
+    public name: TaskName,
+    public userId: UserId,
+    public dueDate: Date
+  ) {}
+
+  /**
+   * タスクを生成する
+   */
+  static create(name: TaskName, userId: UserId, dueDate: Date): Task {
+    const id = new TaskId();
+    return new Task(id, name, userId, dueDate);
+  }
+}
+
+/**
+ * タスクID
+ */
+export class TaskId {
+  constructor(public val = uuidv4()) {}
+}
+
+/**
+ * タスク名
+ */
+export class TaskName {
+  constructor(public val: string) {
+    if (val.length < 0 || val.length >= 30) {
+      throw new Error("タスク名は0〜30文字で指定してください");
+    }
+  }
+}
+
+/**
+ * リポジトリのインターフェース
+ */
+export interface TaskRepository {
+  findById(taskId: TaskId): Task;
+  insert(task: Task): void;
+  update(task: Task): void;
+}
