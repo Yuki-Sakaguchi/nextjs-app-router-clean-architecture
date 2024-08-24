b70b2204a5f408d61182544dd00cf752974c17c6
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2024-08-24 09:32:15 +0900

    ページから server action を使ってタスクを作れるように忠誠

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
-import { TaskList } from "@/components/TaskList";
 import { TaskUseCase } from "@/usecase";
+import { TaskList } from "@/components/TaskList";
+import { submit } from "./action";
 
 export default async function Home() {
   const tasks = await new TaskUseCase().getAll();
   return (
-    <div className="m-4">
-      <TaskList tasks={tasks} />
-    </div>
+    <main className="m-4">
+      <form action={submit}>
+        <div>
+          <input
+            type="text"
+            name="taskName"
+            placeholder="task name"
+            className="border border-gray-200 rounded-md py-2 px-3"
+          />
+        </div>
+        <div className="mt-2">
+          <button className="rounded-md bg-black text-white py-2 px-3 transition-colors hover:bg-gray-800">
+            create task
+          </button>
+        </div>
+      </form>
+      <hr className="mt-4" />
+      <div className="mt-4">
+        {tasks.length === 0 ? (
+          <p>タスクがありません</p>
+        ) : (
+          <TaskList tasks={tasks} />
+        )}
+      </div>
+    </main>
   );
 }
